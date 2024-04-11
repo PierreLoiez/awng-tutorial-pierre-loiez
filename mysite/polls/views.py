44bcc1738a05163c09c67b300c0d629c8aa9f0a4
LOIEZ Pierre <p.loiez@pcb61-08-10.isae.fr>
2024-04-11 12:49:35 +0200

    Bugs fixed

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -8,33 +8,15 @@ from django.shortcuts import render, get_object_or_404
 from django.views import generic
 from django.urls import reverse
 from django.utils import timezone
-
+from django.db.models import F
 
 
 # Create your views here.
 
-def index(request):
-    latest_question_list = Question.objects.filter(pub_date__lte=timezone.now()).order_by("-pub_date")[:5]
-    context = {
-        "latest_question_list":latest_question_list
-    }
-    return render(request, "polls/index.html", context)
-
-def detail(request, question_id):
-    try:
-        question = Question.objects.get(pk=question_id)
-    except Question.DoesNotExist:
-        raise Http404("Question does not exist")
-    return render(request, "polls/detail.html", {"question":question})
-
-def results(request, question_id):
-    response = "This is question %s"
-    return HttpResponse(response % question_id)
-
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     try:
-        selected_choice = question.choice.get(pk=request.POST["choice"])
+        selected_choice = question.choice_set.get(pk=request.POST["choice"])
     except(KeyError, Choice.DoesNotExist):
         return render(
             request, 
@@ -46,7 +28,7 @@ def vote(request, question_id):
         )
     
     else:
-        selected_choice.votes+=1
+        selected_choice.votes = F("votes") + 1
         selected_choice.save()
         return HttpResponseRedirect(reverse("polls:results", args=(question.id,)))
     

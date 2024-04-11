5c9aeafe1f94dcff4faabaada34722e2229b8de6
LOIEZ Pierre <p.loiez@pcb61-08-10.isae.fr>
2024-04-11 11:10:50 +0200

    half tuto 5

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,10 +1,15 @@
+from typing import Any
+from django.db.models.query import QuerySet
 from django.shortcuts import render
 from django.http import HttpResponse, Http404, HttpResponseRedirect
 from .models import Question, Choice
 from django.template import loader
 from django.shortcuts import render, get_object_or_404
-
+from django.views import generic
 from django.urls import reverse
+from django.utils import timezone
+
+
 
 # Create your views here.
 
@@ -44,4 +49,19 @@ def vote(request, question_id):
         selected_choice.votes+=1
         selected_choice.save()
         return HttpResponseRedirect(reverse("polls:results", args=(question.id,)))
+    
+class IndexView(generic.ListView):
+    template_name = "polls/index.html"
+    context_object_name = "latest_question_list"
+    
+    def get_queryset(self):
+        return Question.objects.filter(pub_date__lte=timezone.now()).order_by("-pub_date")[:5]
+    
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = "polls/detail.html"
+    
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
     
